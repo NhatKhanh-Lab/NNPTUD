958651e389f3d586cf54a9145f66a3c6a999be7b
PhamMan2004 <leman27052004@gmail.com>
2026-04-07 01:15:59 +0700

    Use PUT for notification order and payment updates

---
+++ src/routes/orderRoutes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const auth = require("../middlewares/auth");
+const authorize = require("../middlewares/authorize");
+const validate = require("../middlewares/validate");
+const catchAsync = require("../utils/catchAsync");
+const controller = require("../controllers/orderController");
+const { createOrderSchema, updateOrderStatusSchema } = require("../validators/orderValidators");
+
+const router = express.Router();
+
+router.use(catchAsync(auth));
+router.get("/", catchAsync(controller.getOrders));
+router.get("/:id", catchAsync(controller.getOrderById));
+router.post("/", authorize("customer"), validate(createOrderSchema), catchAsync(controller.createOrderFromCart));
+router.put(
+  "/:id/status",
+  authorize("admin", "staff"),
+  validate(updateOrderStatusSchema),
+  catchAsync(controller.updateOrderStatus)
+);
+
+module.exports = router;
